db6dcb496116d0c75ea0b28142c942f3a0222de7
Tiago Pache <tpache@turimbr.com>
2016-04-20 21:46:58 -0300

    Testando configuração de retorno padrão para JSON pela API

--- Desafio.API/App_Start/WebApiConfig.cs
+++ Desafio.API/App_Start/WebApiConfig.cs
@@ -13,7 +13,40 @@ namespace Desafio.API
             //config.SuppressDefaultHostAuthentication();
             //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
             config.EnableCors();
-            
+
+            // Web API configuration and services
+
+            var corsConfig = new WebApiCorsConfiguration();
+
+            // this adds the CorsMessageHandler to the HttpConfiguration's
+            // MessageHandlers collection
+            //corsConfig.RegisterGlobal(config);
+
+
+            //corsConfig.AllowAll();
+
+            //// Web API routes
+            //config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}/{format}",
+            //    defaults: new { id = RouteParameter.Optional, format = RouteParameter.Optional }
+            //);
+
+            //config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            //config.Formatters.JsonFormatter
+            //            .SerializerSettings
+            //            .ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+
+            ////config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
 
             // Web API routes
             config.MapHttpAttributeRoutes();
