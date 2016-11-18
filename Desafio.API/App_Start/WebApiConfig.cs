098bf6502d14d018821ed61591219d3dd6855d27
Tiago Pache <tiagopache@gmail.com>
2016-11-18 19:02:51 -0200

    Trying to fix exception telemetry data capture

--- Desafio.API/App_Start/WebApiConfig.cs
+++ Desafio.API/App_Start/WebApiConfig.cs
@@ -1,9 +1,5 @@
-﻿using Desafio.API.Filters;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web.Http;
 
 namespace Desafio.API
@@ -47,6 +43,8 @@ namespace Desafio.API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Filters.Add(new ErrorHandler.AiExceptionFilterAttribute());
         }
     }
 }
--- Desafio.API/ErrorHandler/AiHandleErrorAttribute.cs
+++ Desafio.API/ErrorHandler/AiHandleErrorAttribute.cs
@@ -1,1 +1,20 @@
-using System;using System.Web.Mvc;using Microsoft.ApplicationInsights;namespace Desafio.API.ErrorHandler{    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]     public class AiHandleErrorAttribute : HandleErrorAttribute    {        public override void OnException(ExceptionContext filterContext)        {            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)            {                //If customError is Off, then AI HTTPModule will report the exception                if (filterContext.HttpContext.IsCustomErrorEnabled)                {                       var ai = new TelemetryClient();                    ai.TrackException(filterContext.Exception);                }             }            base.OnException(filterContext);        }    }}
+using Microsoft.ApplicationInsights;using System;using System.Web.Http.Filters;
+using System.Web.Mvc;
+namespace Desafio.API.ErrorHandler{    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class AiHandleErrorAttribute : HandleErrorAttribute    {        public override void OnException(ExceptionContext filterContext)        {            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)            {
+                //If customError is Off, then AI HTTPModule will report the exception
+                if (filterContext.HttpContext.IsCustomErrorEnabled)                {
+                    var ai = new TelemetryClient();                    ai.TrackException(filterContext.Exception);                }
+            }            base.OnException(filterContext);        }    }    public class AiExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (actionExecutedContext != null && actionExecutedContext.Exception != null)
+            {  //or reuse instance (recommended!). see note above 
+                var ai = new TelemetryClient();
+                ai.TrackException(actionExecutedContext.Exception);
+            }
+
+            base.OnException(actionExecutedContext);
+        }
+    }}
